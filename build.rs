504260578cb1865db5e349cb42a55a3e4bb1c239
bitwave <github@oomlu.de>
2021-07-19 19:56:47 +0200

    added PIC feature

--- build.rs
+++ build.rs
@@ -13,6 +13,8 @@ fn compile_libunwind() {
     cfg.flag("-fstrict-aliasing");
     cfg.flag("-fvisibility=hidden");
     cfg.flag("-funwind-tables");
+    #[cfg(feature = "pic")]
+    cfg.pic(true);
     cfg.define("_LIBUNWIND_NO_HEAP", None);
     cfg.define("_LIBUNWIND_IS_BAREMETAL", None);
     cfg.define("_LIBUNWIND_IS_NATIVE_ONLY", None);
